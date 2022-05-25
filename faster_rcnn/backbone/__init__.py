a3ab9a381d809df8861e6e03018b9bb6d2a69a0a
AokidY <104717867+AokidY@users.noreply.github.com>
2022-05-25 20:38:22 +0800

    Update __init__.py

--- faster_rcnn/backbone/__init__.py
+++ faster_rcnn/backbone/__init__.py
@@ -1,4 +1,5 @@
 from .resnet50_fpn_model import resnet50_fpn_backbone
+from .resnet50_fpn_model import mobilenet_v3_large_fpn_backbone
 from .mobilenetv2_model import MobileNetV2
 from .vgg_model import vgg
 from .feature_pyramid_network import LastLevelMaxPool, BackboneWithFPN
